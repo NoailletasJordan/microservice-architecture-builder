4a437d12b473330e286bceb680f7d5b13d7de1f2
NoailletasJordan <j.noailletas@gmail.com>
2024-04-26 10:05:03 +0700

    [Feature #4] Edges can be deleted

---
+++ src/pages/BoardPage/components/CustomEdge/components/Line/index.tsx
@@ -0,0 +1,31 @@
+interface ILine {
+  d: string
+  stroke: string
+  animated?: boolean
+  // Use Exclude to exclude specific properties from the index signature
+  [pathProps: string]: React.SVGProps<SVGPathElement> | any
+}
+
+export default function Line({ d, stroke, animated, ...pathProps }: ILine) {
+  return (
+    <path
+      fill="none"
+      stroke={stroke}
+      d={d}
+      strokeWidth="1"
+      strokeDasharray="10,10"
+      strokeDashoffset="0"
+      {...pathProps}
+    >
+      {animated && (
+        <animate
+          attributeName="stroke-dashoffset"
+          from="0"
+          to="20"
+          dur="1s"
+          repeatCount="indefinite"
+        />
+      )}
+    </path>
+  )
+}
--- src/pages/BoardPage/components/CustomEdge/index.tsx
+++ src/pages/BoardPage/components/CustomEdge/index.tsx
@@ -1,71 +1,85 @@
-import { EdgeProps, getSmoothStepPath } from 'reactflow'
+import { EdgeProps, getSmoothStepPath, useReactFlow } from 'reactflow'
+import EdgeActions from './components/Line/EdgeActions'
+import Line from './components/Line/index'
 
 const OFFSET_DOUBLE = 2
 // svg gradient dont get rendered on straight lines
 // https://stackoverflow.com/a/34687362
 const NO_STRAIGHT_LINE_OFFSET = 0.001
 
-export default function CustomEdge(props: EdgeProps) {
-  const { sourceX, sourceY, targetX, targetY, sourcePosition, targetPosition } =
-    props
+export default function CustomEdgeWrapper({
+  targetedEdge,
+}: {
+  targetedEdge: string | null
+}) {
+  return function CustomEdge(props: EdgeProps) {
+    const {
+      id,
+      sourceX,
+      sourceY,
+      targetX,
+      targetY,
+      sourcePosition,
+      targetPosition,
+    } = props
+    const { setEdges } = useReactFlow()
 
-  const [calcBezierpath] = getSmoothStepPath({
-    sourceX: sourceX,
-    sourceY: sourceY - OFFSET_DOUBLE,
-    sourcePosition,
-    targetPosition,
-    targetX,
-    targetY: targetY - OFFSET_DOUBLE + NO_STRAIGHT_LINE_OFFSET,
-  })
-  const [calcBezierpath2] = getSmoothStepPath({
-    sourceX: targetX,
-    sourceY: targetY + OFFSET_DOUBLE,
-    sourcePosition: targetPosition,
-    targetPosition: sourcePosition,
-    targetX: sourceX,
-    targetY: sourceY + OFFSET_DOUBLE + NO_STRAIGHT_LINE_OFFSET,
-  })
+    const [bezierPathUpper, labelX, labelY] = getSmoothStepPath({
+      sourceX,
+      sourceY: sourceY - OFFSET_DOUBLE,
+      sourcePosition,
+      targetPosition,
+      targetX,
+      targetY: targetY - OFFSET_DOUBLE + NO_STRAIGHT_LINE_OFFSET,
+    })
+    const [bezierPathLower] = getSmoothStepPath({
+      sourceX: targetX,
+      sourceY: targetY + OFFSET_DOUBLE,
+      sourcePosition: targetPosition,
+      targetPosition: sourcePosition,
+      targetX: sourceX,
+      targetY: sourceY + OFFSET_DOUBLE + NO_STRAIGHT_LINE_OFFSET,
+    })
+    const [bezierPathCenter] = getSmoothStepPath({
+      sourceX,
+      sourceY,
+      sourcePosition,
+      targetPosition,
+      targetX,
+      targetY,
+    })
 
-  return (
-    <>
-      <defs>
-        <linearGradient id="myGradient" x1="0%" y1="0%" x2="100%" y2="0%">
-          <stop offset="0%" stopColor="red" />
-          <stop offset="100%" stopColor="blue" />
-        </linearGradient>
-      </defs>
-      <path
-        fill="none"
-        stroke="url(#myGradient)"
-        d={calcBezierpath}
-        strokeWidth="1"
-        strokeDasharray="10,10"
-        strokeDashoffset="0"
-      >
-        <animate
-          attributeName="stroke-dashoffset"
-          from="0"
-          to="20"
-          dur="1s"
-          repeatCount="indefinite"
+    const handleDeleteNode = () => {
+      setEdges((edges) => edges.filter((edge) => edge.id !== id))
+    }
+    const showToolTip = targetedEdge === id
+    return (
+      <>
+        <defs>
+          <linearGradient id="myGradient" x1="0%" y1="0%" x2="100%" y2="0%">
+            <stop offset="0%" stopColor="red" />
+            <stop offset="100%" stopColor="blue" />
+          </linearGradient>
+        </defs>
+
+        <Line d={bezierPathUpper} stroke="url(#myGradient)" animated />
+        <Line d={bezierPathLower} stroke="url(#myGradient)" animated />
+        {/* Thick Hidden Line to ease mouseover/select */}
+        <Line
+          d={bezierPathCenter}
+          strokeWidth="7"
+          stroke=""
+          strokeDasharray=""
+          strokeDashoffset=""
         />
-      </path>
-      <path
-        fill="none"
-        stroke="url(#myGradient)"
-        d={calcBezierpath2}
-        strokeWidth="1"
-        strokeDasharray="10,10"
-        strokeDashoffset="0"
-      >
-        <animate
-          attributeName="stroke-dashoffset"
-          from="0"
-          to="20"
-          dur="1s"
-          repeatCount="indefinite"
-        />
-      </path>
-    </>
-  )
+        {showToolTip && (
+          <EdgeActions
+            handleDeleteNode={handleDeleteNode}
+            labelX={labelX}
+            labelY={labelY}
+          />
+        )}
+      </>
+    )
+  }
 }

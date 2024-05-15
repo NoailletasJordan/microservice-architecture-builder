1d6641fe6891f18afb1bac50d72abb06cbe0e681
NoailletasJordan <j.noailletas@gmail.com>
2024-05-15 23:12:29 +0700

    [Feature #20] Module endpoints

--- src/pages/BoardPage/components/AsideModule/components/Modules/index.tsx
+++ src/pages/BoardPage/components/AsideModule/components/Modules/index.tsx
@@ -1,14 +1,21 @@
 import { deepCopy } from '@/pages/BoardPage/helpers'
 import { ScrollArea, Stack } from '@mantine/core'
 import { useViewportSize } from '@mantine/hooks'
-import { useRef } from 'react'
+import { ReactNode, useRef } from 'react'
 import { useReactFlow } from 'reactflow'
+import { ModuleType } from '../../../Board/components/CustomNode/components/AddModuleMenu/moduleConstants'
 import { Datatype, Module, TCustomNode } from '../../../Board/constants'
+import ModuleEndpoints from './components/ModuleEndpoints'
 import ModuleRichTextEditor from './components/ModuleRichTextEditor'
 
 interface Props {
   modules: Module[]
   serviceId: Datatype['id']
+}
+
+const getComponents: Record<ModuleType, (props: any) => ReactNode> = {
+  markdown: (props) => <ModuleRichTextEditor {...props} />,
+  endpoints: (props) => <ModuleEndpoints {...props} />,
 }
 
 export default function Modules({ serviceId, modules }: Props) {
@@ -37,13 +44,15 @@ export default function Modules({ serviceId, modules }: Props) {
     <div ref={scrollerRef}>
       <ScrollArea h={scrollableHeight}>
         <Stack p="md">
-          {modules.map((module) => (
-            <ModuleRichTextEditor
-              key={module.id}
-              module={module}
-              updateModule={updateModule(module.id)}
-            />
-          ))}
+          {modules.map((module) => {
+            const component = getComponents[module.moduleType]({
+              key: module.id,
+              module,
+              updateModule: updateModule(module.id),
+            })
+
+            return component
+          })}
         </Stack>
       </ScrollArea>
     </div>
--- src/pages/BoardPage/components/Board/components/CustomNode/components/AddModuleMenu/moduleConstants.tsx
+++ src/pages/BoardPage/components/Board/components/CustomNode/components/AddModuleMenu/moduleConstants.tsx
@@ -1,22 +1,36 @@
-import { IconMarkdown } from '@tabler/icons-react'
+import { IconApi, IconMarkdown } from '@tabler/icons-react'
 import { v4 } from 'uuid'
 import { Module } from '../../../../constants'
 
-export type ModuleType = 'markdown'
+export type ModuleType = 'markdown' | 'endpoints'
 export type ModuleConfigData = {
   label: string
   icon: JSX.Element
   getNew: (parentId: Module['id']) => Module
 }
+
+const ICON_STROKE = 1.5
 export const moduleConfig: Record<ModuleType, ModuleConfigData> = {
   markdown: {
-    label: 'Markdown',
-    icon: <IconMarkdown size="md" stroke={1.5} />,
+    label: 'Note',
+    icon: <IconMarkdown size="md" stroke={ICON_STROKE} />,
     getNew: (parentId) => {
       return {
         id: v4(),
-        data: {},
+        data: { text: '' },
         moduleType: 'markdown',
+        parentId,
+      }
+    },
+  },
+  endpoints: {
+    label: 'Endpoints',
+    icon: <IconApi size="md" stroke={ICON_STROKE} />,
+    getNew: (parentId) => {
+      return {
+        id: v4(),
+        data: { endpoints: [{ address: 'users', method: 'GET' }] },
+        moduleType: 'endpoints',
         parentId,
       }
     },

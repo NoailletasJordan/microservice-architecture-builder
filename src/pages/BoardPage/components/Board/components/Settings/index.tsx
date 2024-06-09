94198aadc38a82bd05409dd1c605ad1046d725b2
NoailletasJordan <j.noailletas@gmail.com>
2024-06-09 21:09:05 +0700

    [Feature #38] Add share button in settings

--- src/pages/BoardPage/components/Board/components/Settings/index.tsx
+++ src/pages/BoardPage/components/Board/components/Settings/index.tsx
@@ -2,13 +2,15 @@ import { ActionIcon, Divider, Menu } from '@mantine/core'
 import { IconMenu2 } from '@tabler/icons-react'
 import { Panel } from 'reactflow'
 import ResetBoard from './components/ResetBoard'
+import ShareModalButton from './components/ShareModalButton'
 import SwitchDropHints from './components/SwitchDropHints'
 
 interface Props {
   openResetModal: () => void
+  openShareModal: () => void
 }
 
-export default function Settings({ openResetModal }: Props) {
+export default function Settings({ openResetModal, openShareModal }: Props) {
   const target = (
     <Menu.Target>
       <ActionIcon size="lg" variant="light">
@@ -22,6 +24,7 @@ export default function Settings({ openResetModal }: Props) {
       <Menu shadow="md">
         {target}
         <Menu.Dropdown>
+          <ShareModalButton openShareModal={openShareModal} />
           <ResetBoard openResetModal={openResetModal} />
 
           <Divider my="xs" />

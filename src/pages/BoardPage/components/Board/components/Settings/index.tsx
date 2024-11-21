2792d7a285494721e680af3b2095c73cd44cd282
NoailletasJordan <j.noailletas@gmail.com>
2024-11-21 21:31:27 +0700

    wip

--- src/pages/BoardPage/components/Board/components/Settings/index.tsx
+++ src/pages/BoardPage/components/Board/components/Settings/index.tsx
@@ -2,7 +2,7 @@ import CustomActionIconDefault from '@/components/CustomActionIconDefault'
 import { clickCanvaContext } from '@/contexts/ClickCanvaCapture/constants'
 import { ICON_STYLE } from '@/pages/BoardPage/configs/constants'
 import { Divider, Menu } from '@mantine/core'
-import { useClickOutside, useDisclosure } from '@mantine/hooks'
+import { useDisclosure } from '@mantine/hooks'
 import { IconMenu2 } from '@tabler/icons-react'
 import { useContext, useEffect } from 'react'
 import { Panel } from 'reactflow'
@@ -17,7 +17,6 @@ interface Props {
 export default function Settings({ openResetModal }: Props) {
   const { canvaClickIncrement } = useContext(clickCanvaContext)
   const [isOpened, { close, toggle }] = useDisclosure(false)
-  const ref = useClickOutside(close)
 
   useEffect(() => {
     canvaClickIncrement !== 0 && close()
@@ -37,7 +36,7 @@ export default function Settings({ openResetModal }: Props) {
     <Panel position="top-left">
       <Menu shadow="md" opened={isOpened}>
         {target}
-        <Menu.Dropdown p="xs" ref={ref}>
+        <Menu.Dropdown p="xs">
           <ResetBoard openResetModal={openResetModal} />
           <Github />
           <Divider my="xs" />
